1b6b0eca385e50ac1a4d050090d8e987144068d5
Yoann Rey <y.rey@tims.fr>
2022-11-16 17:17:55 +0100

    Adding add and remove characters from word
    Signed-off-by: Yoann Rey <y.rey@tims.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <span>HELLO</span>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const BACKSPACE = 'Backspace'
+    const [pressed, setPressed] = useState<Array<string>>([]);
+    useEffect(() => {
+            function handleKeyDown({key}: { key: string }) {
+                const isChar: boolean = /^[A-Za-z]$/.test(key);
+                const isBackSpace: boolean = key === BACKSPACE;
+                if (isBackSpace) {
+                    setPressed((prev) => {
+                        const temp = [...prev];
+                        temp.pop();
+                        return temp;
+                    })
+                } else if (isChar && pressed.length < 5)
+                    setPressed((prev) => [...prev, key]);
+            }
 
-export default App;
+            window.addEventListener('keydown', handleKeyDown);
+            return () => {
+                window.removeEventListener('keydown', handleKeyDown);
+            }
+        }
+        ,
+        [pressed.length]
+    ) // Used to check when length has changed
+
+    console.log(pressed)
+
+    return (
+        <div>
+            <h1 className='text-3xl font-bold uppercase'>
+                Hello World!
+            </h1>
+            <div>
+                {pressed.map((char, i) => {
+                    return <span key={i} className="uppercase">{char}</span>
+                })}
+            </div>
+        </div>
+    );
+}
