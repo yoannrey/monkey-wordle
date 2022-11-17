743a71692712127389c0f2113ca58bbff5567f18
Yoann Rey <y.rey@tims.fr>
2022-11-17 18:46:36 +0100

    Need to correct css
    Signed-off-by: Yoann Rey <y.rey@tims.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import WordRow, {EmptyRow} from "./Components/Grid/Cell";
+import {Utils} from "./Utils";
 
 export default function App() {
-    const BACKSPACE = 'Backspace'
     const [pressed, setPressed] = useState<Array<string>>([]);
+    const [submitted, setSubmitted] = useState<Array<Array<string>>>([]);
     useEffect(() => {
             function handleKeyDown({key}: { key: string }) {
                 const isChar: boolean = /^[A-Za-z]$/.test(key);
-                const isBackSpace: boolean = key === BACKSPACE;
-                if (isBackSpace) {
-                    setPressed((prev) => {
-                        const temp = [...prev];
-                        temp.pop();
-                        return temp;
-                    })
-                } else if (isChar && pressed.length < 5)
-                    setPressed((prev) => [...prev, key]);
+                const isBackSpace: boolean = key === Utils.BACKSPACE;
+                const isEnter: boolean = key === Utils.ENTER;
+                const isWordComplete: boolean = pressed.length === 5;
+                switch (true) {
+                    case isBackSpace:
+                        setPressed((prev) => {
+                            const temp = [...prev];
+                            temp.pop();
+                            return temp;
+                        });
+                        break;
+                    case isEnter && isWordComplete:
+                        setSubmitted((prev) => [...prev, pressed]);
+                        setPressed([]);
+                        break;
+                    case isChar && pressed.length < 5:
+                        setPressed((prev) => [...prev, key]);
+                        break;
+                }
+                console.log(submitted);
             }
 
             window.addEventListener('keydown', handleKeyDown);
@@ -24,22 +37,16 @@ export default function App() {
             }
         }
         ,
-        [pressed.length]
+        [pressed.length, pressed]
     ) // Used to check when length has changed
-
-    console.log(pressed)
 
     return (
         <div className="h-screen flex flex-col
-                    items-center justify-center">
+                    items-center">
             <div className="content-center">
-                {pressed.map((char, i) => {
-                    return (
-                        <span key={i} className="uppercase test text-gray-base
-                              mr-3 py-8 px-10 h-2 border
-                              border-gray-200 rounded mb-2">{char}</span>
-                    )
-                })}
+                <WordRow value={pressed} />
+                <EmptyRow />
+                <EmptyRow />
             </div>
         </div>
     );
